cb090d43c556dc31b50c4d1500c331e6b7472369
Logan Martlew <logan.martlew@gmail.com>
2022-05-21 14:30:37 +1200

    Added auth service

--- Server/src/java/auth/AuthService.java
+++ Server/src/java/auth/AuthService.java
@@ -4,7 +4,10 @@
  */
 package auth;
 
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateful;
+import user.User;
+import user.UserDAO;
 
 /**
  *
@@ -13,4 +16,28 @@ import jakarta.ejb.Stateful;
 @Stateful
 public class AuthService {
     
+    private String userEmail;
+    
+    @EJB
+    UserDAO userDao;
+    
+    public void login(String email, String password) {
+        User user = userDao.getUser(email);
+        if (password.equals(user.getPassword())) {
+            this.userEmail = user.getEmail();
+        }
+    }
+    
+    public void signup(String email, String password) {
+        User newUser = userDao.createUser(email, password);
+        this.userEmail = newUser.getEmail();
+    }
+    
+    public void logout() {
+        this.userEmail = null;
+    }
+    
+    public boolean isLoggedIn() {
+        return userEmail != null;
+    }
 }
