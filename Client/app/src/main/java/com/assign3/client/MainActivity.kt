7b1165d342f5243e1fed563250ccad7f0611788d
Logan Martlew <logan.martlew@gmail.com>
2022-05-28 15:56:06 +1200

    i hagve no idea

--- Client/app/src/main/java/com/assign3/client/MainActivity.kt
+++ Client/app/src/main/java/com/assign3/client/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.assign3.client
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,29 +9,17 @@ import android.widget.EditText
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var items: ArrayList<String>
-    private lateinit var itemsAdapter: ArrayAdapter<String>
-    private lateinit var lvItems: ListView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // When activity starts prompt for an email and password
-
-        // ADD HERE
-        lvItems = findViewById<ListView>(R.id.lvItems)
-        items = ArrayList<String>()
-        itemsAdapter = ArrayAdapter(this,
-				android.R.layout.simple_list_item_1, items)
-        lvItems.adapter = itemsAdapter
-        items.add("First Item")
-        items.add("Second Item")
     }
 
-    fun onAddItem(view: View) {
-        val etNewItem = findViewById<View>(R.id.etNewItem) as EditText
-        val itemText = etNewItem.text.toString()
-        itemsAdapter.add(itemText)
+    fun handleLogin(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun handleSignup() {
+
     }
 }
